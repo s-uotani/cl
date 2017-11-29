77939e0f97b432e0b462dd3561e660442ddc5aa9
s-uotani <8114013@ed.tus.ac.jp>
2017-11-29 11:30:46 +0900

    gettimeofday

---
+++ time.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+double gettimeofday_sec(){
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec + tv.tv_usec * 1e-6;
+}
+
+int main(void){
+    double stert, end;
+    start=gettimeofday_sec();
+    /*処理*/
+    end=gettimeofday_sec();
+    printf("time\t%lf s\n", end-start);
+    return 0;
+}
